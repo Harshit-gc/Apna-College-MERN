88b073c5b6cf12eecff9182aaa4b200f11945368
Harshit <harshitgargacharya@gmail.com>
2026-07-26 11:40:24 +0530

    Database Relationships Completed

---
+++ DatabaseRelationships/Models/posts.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+main().then(() => console.log("Connection Successful")).catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
+}
+
+const userSchema = new Schema({
+    username: String,
+    email: String
+});
+
+const postSchema = new Schema({
+    content: String,
+    likes: Number,
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+const Post = mongoose.model("Post", postSchema);
+
+const addData = async () => {
+    let user1 = new User({
+        username: "Amit",
+        email: "amit@gmail.com"
+    });
+
+    let post1 = new Post({
+        content: "Hello World",
+        likes: 7
+    });
+    post1.user = user1;
+
+    let res1 = await user1.save();
+    let res2 = await post1.save();
+    console.log(res1);
+    console.log(res2);
+};
+
+addData();
+
---
+++ DatabaseRelationships/Models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+main().then(() => console.log("Connection Successful")).catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
+}
+
+const userSchema = new Schema({
+    username: String,
+    addresses: [
+        {
+            location: String,
+            city: String
+        },
+    ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+const addUsers = async () => {
+    let user1 = new User({
+        username: "AMIT",
+        addresses: [{
+            location: "India",
+            city: "Delhi"
+        }]
+    })
+
+    user1.addresses.push({location: "India", city: "Noida"});
+    let result = await user1.save();
+    console.log(result);
+};
+
+addUsers();
